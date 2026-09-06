0a0afef705b41038f969e3b61df8b7e73fdf8996
muaadhparts <asmurayan@gmail.com>
2026-09-07 00:34:46 +0300

    Align canonical and sitemap origin with deployed website

--- lib/site.ts
+++ lib/site.ts
@@ -1,2 +1,2 @@
-export const SITE_URL = (process.env.SITE_URL || 'https://date-dimension.coral-bud-3395.chatgpt.site').replace(/\/$/,'');
+export const SITE_URL = (process.env.SITE_URL || 'https://date-dimension.atwjry.chatgpt.site').replace(/\/$/,'');
 export const INDEXABLE = process.env.PUBLIC_INDEXING === 'true';
